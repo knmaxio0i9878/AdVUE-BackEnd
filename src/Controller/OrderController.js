6e1d7c5442c0245936902d3ad711743c5cc86c5f
knmaxio0i9878 <pmakwana1908@gmail.com>
2025-03-23 10:38:01 +0530

    ORDER

--- src/Controller/OrderController.js
+++ src/Controller/OrderController.js
@@ -117,10 +117,113 @@ const deleteOrder = async(req,res)=>{
            })
        }
 }
+const updateOrderComplete = async (req, res) => {
+    const id = req.params.id
+    const emailBody = `
+  <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
+    <h3 style="color: #333;">Congratulations !</h3><br/>
+    <h3 style="color: #333;">Your Order with AdVUE advertisement has been successfull.</h3><br/>
 
+    <p>Thank you for chosing us. We will provide you the best deals and more in future also.</p>
+    <p>If you love our services and please give feedback from your profile section.</p>
+
+    
+    
+    <p style="margin-top: 20px; color: #555;">If you have any questions, feel free to contact us.</p> <br />
+
+    <p style="margin-top: 20px; color: #555;">AdVUE Office : 8140952934 </p> <br />
+
+  </div>
+`;
+
+    try {
+        const order = await orderschema.findById(id).populate("user_id")
+        const email = order?.user_id?.email
+        console.log("email",email);
+        const orderstatus = {
+            status: req.body.status,
+            halfamount: req.body.halfamount,
+            remainingamount: req.body.remainingamount
+        }
+
+        const response = await orderschema.findByIdAndUpdate(id, orderstatus)
+        await mail.sendingMail(email,"AdVUE Order Success",emailBody)
+        if (response) {
+            res.status(201).json({
+                data: response,
+                message: 'Order updated Successfully'
+            })
+        }
+        else {
+            res.status(404).json({
+                message: 'No such Order found'
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+
+
+
+}
+const updateOrderCancel = async (req, res) => {
+    const id = req.params.id
+    const emailBody = `
+  <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
+    <h3 style="color: #333;">We are sorry to say you that your order is being cancelled due to unpaid payment !</h3><br/>
+    <h3 style="color: #333;">If you want to continue then pay the remaining payment. The number of days will not be added to your advertisement.</h3><br/>
+    <h3 style="color: #333;">You can pay the remaining amount that we provided bank details on your previous email.</h3><br/>
+    <h3 style="color: #333;">Here is the upi details also : <b><ul>pmakwana1908@ohkhdhc</ul></b></h3><br/>
+
+
+
+    
+    
+    <p style="margin-top: 20px; color: #555;">If you have any questions, feel free to contact us.</p> <br />
+
+    <p style="margin-top: 20px; color: #555;">AdVUE Office : 8140952934 </p> <br />
+
+  </div>
+`;
+
+    try {
+        const order = await orderschema.findById(id).populate("user_id")
+        const email = order?.user_id?.email
+        console.log("email",email);
+        const orderstatus = {
+            status: req.body.status,
+            halfamount: req.body.halfamount,
+            remainingamount: req.body.remainingamount
+        }
+
+        const response = await orderschema.findByIdAndUpdate(id, orderstatus)
+        await mail.sendingMail(email,"AdVUE Order Cancelled",emailBody)
+        if (response) {
+            res.status(201).json({
+                data: response,
+                message: 'Order updated Successfully'
+            })
+        }
+        else {
+            res.status(404).json({
+                message: 'No such Order found'
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+
+}
 module.exports = {
     addOrder,
     getAllOrders,
     getSingleOrder,
-    deleteOrder
+    deleteOrder,
+    updateOrderComplete,
+    updateOrderCancel
+
 }
